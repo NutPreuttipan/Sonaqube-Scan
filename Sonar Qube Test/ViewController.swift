4dd400de5d545782c71385dddcaa0ba5f733bf2c
Preuttipan <terrorize.nut@gmail.com>
2019-09-18 15:00:25 +0700

    initial commit

---
+++ Sonar Qube Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sonar Qube Test
+//
+//  Created by Preuttipan Janpen on 18/9/2562 BE.
+//  Copyright © 2562 preuttipan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
